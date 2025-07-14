fa12ea7f9f253e718ee9e264ebd741d340d784e7
suryo <deaglefrenzy@gmail.com>
2025-07-14 16:12:22 +0800

    jacky maouren

---
+++ jacky-maouren/colors.php
@@ -0,0 +1,11 @@
+<?php
+$groom = "Jacky";
+$bride = "Maouren";
+$judul = $groom . " & " . $bride . "'s Wedding";
+$folder = $groom . "-" . $bride;
+$tabledb = $groom . $bride;
+$warna1 = "#F7F0E6";
+$warna2 = "#FCFCF2";
+$warna3 = "#20130D";
+$warna4 = "#B6910C";
+$warna5 = "#633A11";
